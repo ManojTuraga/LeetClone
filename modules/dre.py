b44e601fc2ff2b3195909d6dbf6292eb7daba20e
Manoj Turaga <manoj.turaga@ku.edu>
2024-10-27 22:28:08 -0500

    finished up commenting

--- modules/dre.py
+++ modules/dre.py
@@ -1,3 +1,42 @@
+'''
+Module: dre.py
+Creation Date: October 26th, 2024
+Author: Manoj Turaga
+Contributors: Manoj Turaga
+
+Description:
+    This module is the Dynamic Runtime Environment. This module will take in
+    code and the language that the code is written in and will provide the
+    result of the execution, whether it was succesful or if there were errors
+    in compilation and/or execution
+
+Inputs:
+    None
+
+Outputs:
+    None
+
+Preconditions:
+    None
+
+Postconditions:
+    None
+
+Error Conditions:
+    None
+
+Side Effects:
+    None
+
+Invariants:
+    None
+
+Known Faults
+    None
+'''
+# NOTE: AT THIS POINT, THE FOLLOWING CLASS HAS WHAT
+# WE CONSIDER TO BE THE FUNCTIONS THAT MIGHT BE USED
+# IN IT'S IMPLEMENTATION. IN NO WAY ARE THEY USED ATM
 class DRE:
     def __init__( self ):
         pass
